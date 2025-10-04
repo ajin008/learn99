452a420a8dc9c1be146b4acf9d38f373b37b1a7c
Ajin <ajinkp9745@gmail.com>
2025-10-04 23:10:38 +0530

    Add dynamic export to Generate Password page for improved rendering

--- src/app/generate-password/page.tsx
+++ src/app/generate-password/page.tsx
@@ -1,5 +1,6 @@
-"use client";
 import GeneratePasswordPage from "../components/GeneratePasswordClient";
+
+export const dynamic = "force-dynamic";
 
 export default function Page() {
   return <GeneratePasswordPage />;
